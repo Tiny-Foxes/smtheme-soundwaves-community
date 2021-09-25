bb51f46dfc98bdd1c3d3d5dba21b216ba8bf73f4
Lirodon <shawn.thompson306@gmail.com>
2021-09-25 17:56:57 -0600

    Refresh the battery lifebar

--- Graphics/LifeMeterBattery lives/default.lua
+++ Graphics/LifeMeterBattery lives/default.lua
@@ -8,19 +8,13 @@ local LifeRatio;
 
 return Def.ActorFrame {
 	LoadActor("_lives")..{
-		InitCommand=cmd(pause;horizalign,left;x,-(barWidth/2));
+		InitCommand=function (self) self:pause():horizalign(left):x(-(barWidth/2)):diffuse(PlayerColor(player)) end,
 		BeginCommand=function(self,param)
-			local screen = SCREENMAN:GetTopScreen();
-			local glifemeter = screen:GetLifeMeter(player);
-				self:setstate(clamp(glifemeter:GetTotalLives()-1,0,9));
-				
-				if glifemeter:GetTotalLives() <= 4 then
-					self:zoomx(barWidth/(4*64));
-				else
-					self:zoomx(barWidth/((glifemeter:GetTotalLives())*64));
-				end
-				self:cropright((640-(((glifemeter:GetTotalLives())*64)))/640);
-		end;
+			local screen = SCREENMAN:GetTopScreen()
+			local glifemeter = screen:GetLifeMeter(player)
+				self:setstate(clamp(glifemeter:GetTotalLives()-1,0,9))
+				self:cropright((640-(((glifemeter:GetTotalLives())*64)))/640)
+		end,
 		LifeChangedMessageCommand=function(self,param)
 			if param.Player == player then
 				if param.LivesLeft == 0 then
@@ -30,13 +24,12 @@ return Def.ActorFrame {
 					self:visible(true)
 				end
 			end
-		end;
+		end,
 		StartCommand=function(self,param)
 			if param.Player == player then
 				self:setstate( clamp(param.LivesLeft-1,0,9) );			
 			end			
-		end;
-		FinishCommand=cmd(playcommand,"Start");
-	};
-
-};
+		end,
+		FinishCommand=function(self) self:playcommand("Start") end
+	}
+}
